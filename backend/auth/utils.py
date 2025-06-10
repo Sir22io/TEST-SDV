7d9b5b538342583b392dba5f0bae4965122f772c
Clément G. <103967887+Sir22io@users.noreply.github.com>
2025-06-10 14:34:55 +0200

    Delete backend directory

--- backend/auth/utils.py
+++
@@ -1,7 +0,0 @@
-import bcrypt
-
-def hash_password(password: str) -> str:
-    return bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
-
-def verify_password(password: str, hash_pw: str) -> bool:
-    return bcrypt.checkpw(password.encode(), hash_pw.encode())
